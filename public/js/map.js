21ef04d36a9481b848519fec316577c1a1d910ea
Your Name <you@example.com>
2026-05-27 18:31:47 +0530

    edited map section

--- public/js/map.js
+++ public/js/map.js
@@ -1,19 +1,26 @@
-if (typeof coordinates !== "undefined") {
+console.log("MAP FILE LOADED");
 
-    const map = L.map('map').setView(
-        [coordinates[0], coordinates[1]],
-        13
-    );
+const mapDiv = document.getElementById("map");
+
+if (mapDiv) {
+
+    const lat = parseFloat(mapDiv.dataset.lat);
+    const lng = parseFloat(mapDiv.dataset.lng);
+    const title = mapDiv.dataset.title;
+
+    console.log(lat, lng);
+
+    const map = L.map("map").setView([lat, lng], 13);
 
     L.tileLayer(
-        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
         {
-            attribution: '&copy; OpenStreetMap contributors'
+            attribution: "&copy; OpenStreetMap contributors",
         }
     ).addTo(map);
 
-    L.marker([coordinates[0], coordinates[1]])
+    L.marker([lat, lng])
         .addTo(map)
-        .bindPopup(listingTitle)
+        .bindPopup(title)
         .openPopup();
 }
